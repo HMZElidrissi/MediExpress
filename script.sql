9afe065a1f866cb90b55893a48fc565e2c62206b
HMZElidrissi <hamza.ezzharelidrissi1@gmail.com>
2023-12-28 12:01:29 +0100

    update sql script

--- script.sql
+++ script.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE IF NOT EXISTS PharmacyDB;
+USE PharmacyDB;
 
 -- Create 'users' table
 CREATE TABLE users (
@@ -25,6 +26,13 @@ CREATE TABLE medicaments (
      quantity_in_stock INT NOT NULL
 );
 
+-- Create 'rapports' table
+CREATE TABLE reports (
+                         id INT AUTO_INCREMENT PRIMARY KEY,
+                         date DATETIME DEFAULT (CURRENT_DATE),
+                         report_type ENUM('vente', 'stock')
+);
+
 -- Create 'ventes' table
 CREATE TABLE sales (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -33,16 +41,40 @@ CREATE TABLE sales (
     medicament_id INT NOT NULL,
     quantity INT NOT NULL,
     sale_type ENUM('En Ligne', 'En Magasin') NOT NULL,
+    report_id INT NOT NULL,
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
     FOREIGN KEY (medicament_id) REFERENCES medicaments(id),
-    FOREIGN KEY (patient_id) REFERENCES patients(patient_id)
+    FOREIGN KEY (report_id) REFERENCES reports(id)
 );
 
--- Create 'rapports' table
-CREATE TABLE reports (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    date DATETIME DEFAULT (CURRENT_DATE),
-    report_type ENUM('vente', 'stock')
-);
+
+-- Insert data into 'users' table
+INSERT INTO users (username, email, password, user_type) VALUES
+('JohnDoe', 'johndoe@example.com', 'pass123', 'patient'),
+('JaneSmith', 'janesmith@example.com', 'pass123', 'admin'),
+('EmmaBrown', 'emmabrown@example.com', 'pass123', 'patient');
+
+-- Insert data into 'patients' table
+INSERT INTO patients (patient_id, patient_type) VALUES
+(1, 'En Ligne'),
+(3, 'En Magasin');
+
+-- Insert data into 'medicaments' table
+INSERT INTO medicaments (name, description, price, quantity_in_stock) VALUES
+('Paracetamol', 'Pain reliever and a fever reducer', 5, 100),
+('Amoxicillin', 'Antibiotic used to treat a number of bacterial infections', 8, 50),
+('Cetirizine', 'Antihistamine used to treat allergies', 3, 75);
+
+-- Insert data into 'rapports' table
+INSERT INTO reports (report_type) VALUES
+('vente'),
+('stock');
+
+-- Insert data into 'ventes' table (assuming reports have been created)
+INSERT INTO sales (patient_id, medicament_id, quantity, sale_type, report_id) VALUES
+(1, 1, 2, 'En Ligne', 1),
+(3, 2, 1, 'En Magasin', 1);
+
 
 /*
  Another approach would be:
